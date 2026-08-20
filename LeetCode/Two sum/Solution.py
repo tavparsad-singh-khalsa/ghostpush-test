e7a8125ef669a9650eb7216960be837d17b83962
GhostPush Test <ghostpush-test@example.com>
2026-08-20 15:31:14 +0530

    feat(leetcode): add Two Sum solution

--- LeetCode/Two sum/Solution.py
+++ LeetCode/Two sum/Solution.py
@@ -1,12 +1,1 @@
-def twoSum(nums, target):
-    seen = {}
-
-    for i, num in enumerate(nums):
-        complement = target - num
-
-        if complement in seen:
-            return [seen[complement], i]
-
-        seen[num] = i
-
-    return []
+pass
